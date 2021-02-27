fb02cc73c6d71e33fc7033195fc8751d1d83d762
mstappers <markstappers@outlook.com>
2021-02-27 12:50:20 +0100

    Create PrintNumber.cpp

---
+++ PrintNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;    
+    return 0;
+}
+
+// Output
+// Enter an integer: 35
+// You entered 35
